80a6f912f0d433aa8ff4b0523d2580eaafa2eb82
Simiamine <mzaliamine@gmail.com>
2024-09-27 15:31:11 +0200

    ajout mac

--- pokeshop/manage.py
+++ pokeshop/manage.py
@@ -2,11 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import platform
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokeshop.settings')
+    # Détection du système d'exploitation et sélection des paramètres appropriés
+    if platform.system() == 'Darwin':  # macOS
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokeshop.settings_mac')
+    elif platform.system() == 'Windows':  # Windows
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokeshop.settings_windows')
+    else:  # Pour d'autres systèmes (Linux ou autres)
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokeshop.settings_windows')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
